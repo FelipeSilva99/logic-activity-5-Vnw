2a77cfddb6a1eb8138994464859b6dfb9ab4efb4
FelipeSilva99 <felipe.rl.silva@hotmail.com>
2021-05-22 19:48:03 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Crie um array que receba 5 itens e exiba no console.
+let Countries = ['China', 'Índia', 'Peru', 'Itália', 'México']
+console.log(Countries)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+Countries.unshift('Jordânia')
+
+// Utilize um método para remover o último nome do array.
+Countries.pop(5)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+Countries.push('Peru', 'Itália')
+
+// Utilize um método para remover o primeiro nome do array.
+Countries.shift(0)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+let NumbList= [7, 5, 6, 3, 8, 9, 2, 1, 4]
+NumbList.sort()
+console.log(NumbList)
